356db1bf42b5028a8af5396f3bb557769f9536ce
Gmero <2431775986@qq.com>
2024-07-13 00:25:56 +0800

    ..

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,22 +2,20 @@ import { useLocation } from '@solidjs/router'
 import { Icon } from '@iconify-icon/solid'
 
 function NavItem(props: { path: string, icon: string, _label: string }) {
+  const location = useLocation()
+  const active = (activeClass: string, fallbackClass: string) => {
+    return location.pathname === props.path ? activeClass : fallbackClass
+  }
   return (
     <li>
-      <a href={props.path} class="group items-center justify-center flex rounded-lg bg-gray-100 hover:bg-blue lh-none w-48px h-48px">
-        <Icon class="color-blue group-hover:color-white" icon={props.icon} width="25px" height="25px" />
+      <a href={props.path} class={`transition group items-center justify-center flex rounded-lg ${active('bg-blue', 'bg-gray-100')} hover:bg-blue lh-none w-48px h-48px`}>
+        <Icon class={`transition ${active('color-white', 'color-blue')}  group-hover:color-white`} icon={props.icon} width="25px" height="25px" />
       </a>
     </li>
-
   )
 }
 
 export default function Nav() {
-  const location = useLocation()
-  const active = (path: string) =>
-    path === location.pathname
-      ? 'border-sky-600'
-      : 'border-transparent hover:border-sky-600'
   return (
     <nav class="bg-base fixed left-0 h-full w-60px b-r">
       <ul class="container flex flex-col items-center py-3 text-gray-200 gap-2">
