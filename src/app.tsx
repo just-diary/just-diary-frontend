0cfabcee6fca90660a0cfa5a0b3a818ff98da95b
Gmero <2431775986@qq.com>
2024-08-13 03:48:08 +0800

    save

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,54 @@
-import { Router } from '@solidjs/router'
+import { BeforeLeaveEventArgs, Router, useBeforeLeave } from '@solidjs/router'
 import { FileRoutes } from '@solidjs/start/router'
-import { Suspense } from 'solid-js'
+import { Suspense, createUniqueId } from 'solid-js'
+
 import GmScroll from './components/GmScroll'
+import LoadingPage from './parts/LoadingPage'
 import Nav from '~/components/Nav'
+
+import '~/style/app.css'
+
+function RouteWrapper(props: {
+  children: any
+}) {
+  const id = createUniqueId()
+  const [isProcessing, setIsProcessing] = createSignal(false)
+  const [status, setStatus] = createSignal('entering')
+  let ref!: HTMLDivElement
+
+  useBeforeLeave((e: BeforeLeaveEventArgs) => {
+    if (!e.defaultPrevented) {
+      // preventDefault to block immediately and prompt user async
+      e.preventDefault()
+      setStatus('exiting')
+      ref.onanimationend = () => {
+        setStatus('exited')
+        setIsProcessing(true)
+        e.retry(true)
+
+        ref.onanimationend = null
+      }
+    }
+  })
+
+  return (
+    <div
+      data-id={id}
+      class="ml-60px p-1 transition ani-route"
+      data-status={status()}
+      ref={ref}
+      onAnimationEnd={() => {
+        if (status().endsWith('ing') && !isProcessing()) {
+          setStatus(status().replace('ing', 'ed'))
+        }
+      }}
+    >
+      <Show when={isProcessing()} fallback={props.children}>
+        <LoadingPage />
+      </Show>
+    </div>
+  )
+}
 
 export default function App() {
   return (
@@ -11,9 +57,7 @@ export default function App() {
         <>
           <Nav />
           <GmScroll height="100vh">
-            <div class="ml-60px p-1">
-              <Suspense>{props.children}</Suspense>
-            </div>
+            <RouteWrapper><Suspense>{props.children}</Suspense></RouteWrapper>
           </GmScroll>
         </>
       )}
---
+++ src/parts/LoadingPage/index.tsx
@@ -0,0 +1,9 @@
+export default function LoadingPage() {
+  console.log(1)
+
+  return (
+    <div class="flex items-center justify-center h-full">
+      <div class="text-2xl font-bold">Loading...</div>
+    </div>
+  )
+}
